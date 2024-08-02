f469b6a6cc6e847e897a452d24c718c18f908f98
Hart102 <hartongit102@gmail.com>
2024-08-02 15:09:08 -0400

    initial commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(cors());
 const server = createServer(app);
 const io = new Server(server, {
   cors: { origin: "*" }, // Adjust CORS as needed
+  transports: ["websocket", "polling"],
 });
 
 io.on("connection", (socket) => {
